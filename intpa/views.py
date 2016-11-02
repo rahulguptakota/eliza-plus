62b3b2d31fcd84de39da319f3e5ef9d2eeaa7e60
Rahul Gupta <grahul@iitk.ac.in>
2016-11-02 17:15:19 +0530

    a working copy

--- intpa/views.py
+++ intpa/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import pygeoip
+import requests
+import json
 # Create your views here.
 def index(request):
     return HttpResponse("hello world")
@@ -9,3 +11,10 @@ def chatpage(request):
 #    template = loader.get_template('intpa/chatpage.html')
     context = {}
     return render(request, 'intpa/chatpage.html', context)
+
+def getweather(request):
+    client_address = request.META['HTTP_X_FORWARDED_FOR']
+    gi = pygeoip.GeoIP('GeoIPCity.dat')
+    city = gi.record_by_addr(client_address).city
+    weather_data = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=b64701c2ef43f513e326906d0ddd0f9f')
+    return HttpResponse(json.dumps(weather_data), content_type='application/json');

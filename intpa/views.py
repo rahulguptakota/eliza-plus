7f2689cd63c2bae9fff7978bc951b4a091f69862
Dhawal Upadhyay <dhawal@iitk.ac.in>
2016-11-08 13:59:32 +0530

    ajax fully working for temperature

--- intpa/views.py
+++ intpa/views.py
@@ -13,10 +13,12 @@ def chatpage(request):
     return render(request, 'intpa/chatpage.html', context)
 
 def getweather(request):
-    client_address = request.META['HTTP_X_FORWARDED_FOR']
-    gi = pygeoip.GeoIP('GeoLiteCity.dat')
-    print client_address
-    city = gi.record_by_addr(client_address).city
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=b64701c2ef43f513e326906d0ddd0f9f')
+    #client_address = request.META['HTTP_X_FORWARDED_FOR']
+    client_address = "103.246.106.9"
+    #gi = pygeoip.GeoIP('GeoLiteCity.dat')
+    #print client_address
+    #city = gi.record_by_addr(client_address)['city']
+#    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+str(city)+'&APPID=b64701c2ef43f513e326906d0ddd0f9f')
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+'Kanpur'+'&APPID=b64701c2ef43f513e326906d0ddd0f9f')
     weather_data = r.json()
     return HttpResponse(json.dumps(weather_data), content_type='application/json');
